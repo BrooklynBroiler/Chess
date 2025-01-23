e101f96a364b6a3b1cdbae44cf07b59e9ae5f45d
BrooklynBroiler <bbcolumbus2022@gmail.com>
2025-01-22 17:50:55 -0700

    Add paramaters to class ChessMovesCalc

--- shared/src/main/java/chess/ChessMovesCalculator.java
+++ shared/src/main/java/chess/ChessMovesCalculator.java
@@ -1,4 +1,13 @@
 package chess;
 
 public class ChessMovesCalculator {
+    ChessBoard chessBoard;
+    ChessPosition chessPosition;
+    public ChessMovesCalculator(ChessBoard chessBoard, ChessPosition chessPosition) {
+        this.chessBoard = chessBoard;
+        this.chessPosition = chessPosition;
+    }
+
+
+
 }

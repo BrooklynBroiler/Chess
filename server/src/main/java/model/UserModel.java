fd10e5d4bcd77a50265e46e9637165b4e8943a2c
BrooklynBroiler <bbcolumbus2022@gmail.com>
2025-03-03 20:48:02 -0700

    add user model

--- server/src/main/java/model/UserModel.java
+++ server/src/main/java/model/UserModel.java
@@ -1,4 +1,44 @@
 package model;
 
+import java.util.Objects;
+
 public class UserModel {
+    private final String username;
+    private final String password;
+    private final String email;
+    UserModel(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(username, userModel.username) && Objects.equals(password, userModel.password) && Objects.equals(email, userModel.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email);
+    }
+
 }
